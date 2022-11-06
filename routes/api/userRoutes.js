961ed7b979c64c3e64741ae46d3d3c6e401e4c03
isayani <isayani95@gmail.com>
2022-11-06 00:51:41 -0400

    init userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addThought,
+  removeThought,
+  addFriend,
+  removeFriend
+} = require('../../controllers/UserController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/thoughts
+router.route('/:userId/thoughts').post(addThought);
+
+// /api/users/:userId/thoughts/:thoughtId
+router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
