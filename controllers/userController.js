480a2b5081b0de9ab4c20a5cf128b02b59e6df8b
isayani <isayani95@gmail.com>
2022-11-09 22:34:28 -0500

    init userControllers w/o add/remove thought

---
+++ controllers/userController.js
@@ -0,0 +1,141 @@
+const { User, Thought } = require("../models");
+
+// Aggregate function to get the number of Users overall
+const allUsers = async () =>
+  User.aggregate()
+    .count("userCount")
+    .then((numberOfUsers) => numberOfUsers);
+
+// Handles userRoutes
+module.exports = {
+  // Get all Users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single User
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "User not found! Please try again." })
+          : res.json({
+              user,
+              allUsers: await allUsers(),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // create a new User
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Update user by id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "User not found! Please try again." })
+          : res.json({ user: "User successfully updated! 🎉" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Delete user by id (cascade thoughts)
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "User not found! Please try again." })
+          : Thought.findOneAndUpdate(
+              { username: req.params.username },
+              { $pull: { username: req.params.username } },
+              { new: true }
+            )
+      )
+      .then((thought) =>
+        !thought
+          ? res
+              .status(404)
+              .json({ message: "User deleted! No thoughts 💭 to delete." })
+          : res.json({
+              message: "User and their thoughts successfully deleted! 🧠🎉",
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add and Remove Thought (?)
+
+  // Add friend by userId
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((friend) =>
+        !friend
+          ? res
+              .status(404)
+              .json({
+                message: "No friend with that ID found! Please try again.",
+              })
+          : res.json(friend)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete friend by userId
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: "User not found! Please try again." })
+          : res.json({ user: "Friend successfully removed 🎉" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+};
