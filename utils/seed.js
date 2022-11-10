f671cd4e545be43df53727c15b4d71a9ad33b5fb
isayani <isayani95@gmail.com>
2022-11-09 23:37:11 -0500

    connection and pseudo code

--- utils/seed.js
+++ utils/seed.js
@@ -1,3 +1,16 @@
 const connection = require('../config/connection');
 const { User, Thought } = require('../models');
 const { username, email, thoughts } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    console.log('connected');
+
+    // Drop existing Users
+    // Drop existing Thoughts
+    // Create empty array to hold Users
+    // Loop through User Data
+    // Insert Many for userData
+    // Insert Many for thoughtData
+})
