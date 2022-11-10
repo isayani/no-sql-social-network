ca667b8cea9848a798b1cd59edcc3e6927af3209
isayani <isayani95@gmail.com>
2022-11-09 23:29:36 -0500

    added seed data

--- utils/data.js
+++ utils/data.js
@@ -1,13 +1,32 @@
 const usernames = [
-
+  "snooki",
+  "jWoww",
+  "theSitch",
+  "paulyD",
+  "sammiSweetheart",
+  "vinny",
+  "ronnie",
+  "deena",
 ];
 
 const email = [
-
+  "snooki@theshore.net",
+  "jWoww@theshore.net",
+  "theSitch@theshore.net",
+  "paulyD@theshore.net",
+  "sammiSweetheart@theshore.net",
+  "vinny@theshore.net",
+  "ronnie@theshore.net",
+  "deena@theshore.net",
 ];
 
 const thoughts = [
-
+  "Gym, Tan, Laundry!",
+  "She's too young for you bro.",
+  "I'm not good with time. Like, if I ask you the time and you say A quarter to 2, I wouldn't know. Why can't you just say 2:30?",
+  "Tall, completely jacked, steroids, like multiple growth hormones. That's, like, the type that I'm attracted to.",
+  "If you're not a Guido then you can get the heck outta my face.",
+  "Men may be from Mars and women from Venus, but I'm from the Jersey Shore.",
 ];
 
-module.exports = { usernames, email, thoughts}
+module.exports = { usernames, email, thoughts };
