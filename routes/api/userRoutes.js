346e533d44f0bac71f485e826adbacdd897c8509
isayani <isayani95@gmail.com>
2022-11-10 22:12:53 -0500

    removed unneeded routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,8 +5,6 @@ const {
   createUser,
   updateUser,
   deleteUser,
-  addThought,
-  removeThought,
   addFriend,
   removeFriend
 } = require('../../controllers/userController');
@@ -16,12 +14,6 @@ router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
-
-// /api/users/:userId/thoughts
-router.route('/:userId/thoughts').post(addThought);
-
-// /api/users/:userId/thoughts/:thoughtId
-router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
 
 // /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
